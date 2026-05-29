73b4abb0e381964b67aa833971d0e6bf9db4445e
Eaint Wunna Aung <149416294+Scarlettw3241@users.noreply.github.com>
2026-05-29 23:50:21 +0800

    Add files via upload

---
+++ Week7Task2/search_form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Search Cars</title>
+</head>
+<body>
+
+<form action="search_result.php" method="GET">
+    
+    <label>Search Make:</label>
+    <input type="text" name="model">
+
+    <input type="submit" value="Search">
+
+</form>
+
+</body>
+</html>
